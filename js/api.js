8e0ebd708fe3ff03768ebf841af2c08a9e44dbab
Gelacki <gelacki.chowa@gmail.com>
2026-09-24 22:44:25 -0300

    Cria requisicao para editar pensamentos

--- js/api.js
+++ js/api.js
@@ -3,8 +3,7 @@ const api = {
     try {
       const response = await fetch("http://localhost:3000/pensamentos");
       return await response.json();
-    } 
-    catch {
+    } catch {
       alert("Erro ao buscar pensamentos");
       throw error;
     }
@@ -15,18 +14,43 @@ const api = {
       const response = await fetch("http://localhost:3000/pensamentos", {
         method: "POST",
         headers: {
-          "Content-Typ": "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(pensamento),
+      });
+      return await response.json();
+    } catch {
+      alert("Erro ao salvar pensamentos");
+      throw error;
+    }
+  },
+
+  async buscarPensamentoPorId(id) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${id}`);
+      return await response.json();
+    } catch {
+      alert("Erro ao buscar pensamento");
+      throw error;
+    }
+  },
+
+  async editarPensamento(pensamento) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(pensamento),
       });
       return await response.json();
     } 
     catch {
-      alert("Erro ao buscar pensamentos");
+      alert("Erro ao editar pensamentos");
       throw error;
     }
   },
 };
 
 export default api;
-    
